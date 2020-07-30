1a3247e6bdc74cdd8adde621d699e29916d69e1d
Jozsef M Komjati <jm.komjati@codegarager.net>
2020-07-30 23:42:47 +0100

    fix: config

--- client/packages/app/src/config.ts
+++ client/packages/app/src/config.ts
@@ -1,4 +1,8 @@
 import packageJson from "../package.json";
 
+console.log(process.env.REACT_APP_GRAPH_URI);
+
 export const APP_VERSION = packageJson.version;
-export const GRAPH_URI = process.env.REACT_APP_GRAPH_URI;
+// export const GRAPH_URI = process.env.REACT_APP_GRAPH_URI;
+
+export const GRAPH_URI = "http://localhost:5000/graphql";
